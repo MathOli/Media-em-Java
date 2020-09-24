261a33672b3d71f60abc51ce3f06222eec8e809d
MathOli <matheus.oliveira2100@hotmail.com>
2020-09-23 21:59:57 -0300

    Media feita

---
+++ media/src/main/java/br/fatec/media/model/Aluno.java
@@ -0,0 +1,39 @@
+package br.fatec.media.model;
+
+public class Aluno {
+	private String nome;
+	private float p1;
+	private float p2;
+	private float media;
+	public Aluno() {
+
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public float getP1() {
+		return p1;
+	}
+	public void setP1(float p1) {
+		this.p1 = p1;
+	}
+	public float getP2() {
+		return p2;
+	}
+	public void setP2(float p2) {
+		this.p2 = p2;
+	}
+	public float getMedia() {
+		return media;
+	}
+	public void setMedia(float media) {
+		this.media = media;
+	}
+	
+	
+	
+	 
+}
